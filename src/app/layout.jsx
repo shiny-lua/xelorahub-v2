2fd377ee8386b25a40c12ef05429280579becc75
shiny-lua <sol.trx.bf@gmail.com>
2025-07-25 07:04:26 +0900

    delete: chat component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -48,7 +48,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Chat />
+        {/* <Chat /> */}
       </body>
     </html>
   );
